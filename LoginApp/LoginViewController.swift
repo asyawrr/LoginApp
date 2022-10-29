8dfa1863ee630b23eed28abf5f59cd49f053d07b
Asya Sher <asia.kondairy@gmail.com>
2022-10-29 20:46:35 +0300

    1. setup alert buttons 2. valid atetextfields for log in button

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,72 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Asya Sher on 29.10.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var passwordTF: UITextField!
+    @IBOutlet var usernameTF: UITextField!
+    
+    
+    @IBOutlet var logInButton: UIButton!
+    
+    @IBOutlet var forgotNameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func forgotPassButtonPressed() {
+        showAlertHelp(with: "Ooops!", and: "Your password is \"Password\"")
+    }
+    @IBAction func forgotNameButtonPressed() {
+        showAlertHelp(with: "Ooops!", and: "Your name is \"User\"")
+    }
+    
+    @IBAction func logInButtonPressed() {
+        guard let inputName = usernameTF.text, inputName == "User" else {
+            showAlert(with: "Invalid login or password",
+                      and: "Please, enter correct login or password",
+                      for: passwordTF)
+            return
+        }
+        guard let inputPassword = passwordTF.text, inputPassword == "Password" else {
+            showAlert(with: "Invalid login or password",
+                      and: "Please, enter correct login or password",
+                      for: passwordTF)
+            return
+        }
+        
+        }
+}
+
+
+// MARK: extensions
+extension LoginViewController {
+    private func showAlert(with title: String, and massage: String, for textField: UITextField) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField.text = ""
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    private func showAlertHelp(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Asya Sher on 29.10.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    var usernameValue: String!
+
+    @IBOutlet var logOutButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        gradientBackground()
+
+    }
+    
+    
+    @IBAction func logOutButtonPressed() {
+        dismiss(animated: true)
+    }
+    
+    // MARK: private functions
+    
+    private func gradientBackground() {
+        let colorTop = UIColor(red: 100/255, green: 210/255, blue: 255/255, alpha: 0.5).cgColor
+        let colorBottom = UIColor(red: 94/255, green: 92/255, blue: 230/255, alpha: 0.5).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+            gradientLayer.colors = [colorTop, colorBottom]
+            gradientLayer.locations = [0.0, 1.0]
+            gradientLayer.frame = self.view.bounds
+                    
+            self.view.layer.insertSublayer(gradientLayer, at:0)
+        
+        
+    }
+
+}
